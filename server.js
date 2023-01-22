65b314b098f68c54a17abf6d1f7804fca916d801
hialdev <al.developer12@gmail.com>
2023-01-23 03:37:28 +0700

    revision server js

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ require('dotenv').config();
 const express = require('express');
 const next = require('next');
 
-const hostname = process.env.APP_URL || 'localhost';
 const dev = process.env.NODE_ENV !== 'production';
 const port = parseInt(process.env.APP_PORT, 10) || 3000;
-const app = next({ dev, hostname, port });
+
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     const server = express();
+
+    if (!dev) {
+        // Enforce SSL & HSTS in production
+        server.use(function(req, res, next) {
+          var proto = req.headers["x-forwarded-proto"];
+          if (proto === "https") {
+            res.set({
+              'Strict-Transport-Security': 'max-age=31557600' // one-year
+            });
+            return next();
+          }
+          res.redirect("https://" + req.headers.host + req.url);
+        });
+    }
 
     const cases = require('./config/api/case');
     server.use('/api',cases);
@@ -21,12 +35,12 @@ app.prepare().then(() => {
     server.use('/api',services);
 
     server.get('*', (req, res) => {
-        return handle(req, res);
+        handle(req, res);
     });
 
     server.listen(port, (err) => {
         if (err) throw err;
-        console.log('> Ready on http://'+hostname+':'+port);
+        console.log('> Ready on http://localhost:'+port);
     });
 
 });
