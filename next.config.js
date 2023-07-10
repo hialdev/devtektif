6e96e0b8c5d6d7cd54e9809c1f2538b3c9c91ab3
hialdev <al.developer12@gmail.com>
2023-07-10 15:28:11 +0700

    optimation to plesk cpanel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    workerThreads:false,
+    cpus: 4
+  }
 }
 
 module.exports = nextConfig
